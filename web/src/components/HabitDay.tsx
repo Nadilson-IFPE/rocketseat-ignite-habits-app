b53f02351f95ff55818df4ad6538f51db42f2f71
Nadilson <nadilson@protonmail.com>
2023-01-20 06:15:39 -0300

    Tabela de hábitos diários

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,12 +1,8 @@
-interface HabitsProps {
-    completed: number;
-}
-
-export function Habit(props: HabitsProps) {
+export function HabitDay() {
 
     return (
         <div className="bg-zinc-900 w-10 h-10 text-white rounded m-2 flex items-center justify-center">
-            {props.completed}
+
         </div>
     )
 }
