1cdf66c021ff412235689e04dd746002bdd0fe83
Nadilson <nadilson@protonmail.com>
2023-01-20 06:40:29 -0300

    Preenchimento de dias no fim

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -13,6 +13,8 @@ const weekDays = [
 
 const summaryDates = generateDatesFromYearBeginning()
 
+const minimumSummaryDatesSize = 18 * 7 // 18 semanas
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
 
 
 export function SummaryTable() {
@@ -35,6 +37,12 @@ export function SummaryTable() {
                 {summaryDates.map((date) => {
                     return (
                         <HabitDay key={date.toString()} />
+                    )
+                })}
+
+                {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+                    return (
+                        <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
                     )
                 })}
             </div>
