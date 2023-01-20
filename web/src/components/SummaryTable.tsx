ac59b4c4494b326d9d4accbbbdec5c3475cbc7d0
Nadilson <nadilson@protonmail.com>
2023-01-20 13:41:52 -0300

    Popover do detalhe do dia, Progress Bar no quadrado do hábito

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -36,7 +36,9 @@ export function SummaryTable() {
             <div className="grid grid-rows-7 grid-flow-col gap-3">
                 {summaryDates.map((date) => {
                     return (
-                        <HabitDay key={date.toString()} />
+                        <HabitDay key={date.toString()} 
+                        completed={Math.round(Math.random() * 5)} 
+                        amount={5} />
                     )
                 })}
 
