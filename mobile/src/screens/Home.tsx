bb77b60c3fcdbe703fddb391abbd109f23cdae3f
Nadilson <nadilson@protonmail.com>
2023-01-20 10:09:22 -0300

    Dayjs, data no formato pt-BR, range entre datas, cabeçalho de dias da semana

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,8 +1,12 @@
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import { HabitDay, DAY_SIZE } from '../components/HabitDay';
 import { Header } from '../components/Header';
+import { generateRangeDatesFromYearStart } from '../utils/generate-range-between-dates';
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+const datesFromYearStart = generateRangeDatesFromYearStart();
+const minimunSummaryDatesSizes = 18 * 5;
+const amountOfDaysToFill = minimunSummaryDatesSizes - datesFromYearStart.length
 
 export function Home() {
     return (
@@ -23,7 +27,30 @@ export function Home() {
                 }
             </View>
 
-            <HabitDay />
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ paddingBottom: 100 }}
+            >
+                <View className='flex-row flex-wrap'>
+                    {
+                        datesFromYearStart.map(date => (
+                            <HabitDay key={date.toISOString()} />
+                        ))
+                    }
+
+                    {
+                        amountOfDaysToFill > 0 && Array
+                            .from({ length: amountOfDaysToFill })
+                            .map((_, index) => (
+                                <View
+                                    key={index}
+                                    className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                                    style={{ width: DAY_SIZE, height: DAY_SIZE }}
+                                />
+                            ))
+                    }
+                </View>
+            </ScrollView>
         </View>
     )
 } 
