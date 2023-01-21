6f8f9fc1076f35d91eb01a1f7ddb2c6698e50d42
Nadilson <nadilson@protonmail.com>
2023-01-21 04:29:15 -0300

    Componente BackButton

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,9 +1,10 @@
 import { View } from 'react-native';
+import { BackButton } from '../components/BackButton';
 
 export function New() {
     return (
         <View className='flex-1 bg-background px-8 pt-16'>
-
+            <BackButton />
         </View>
     )
 }
