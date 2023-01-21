78ada8621d00691bb2feac94acac3ffa6edfb83d
Nadilson <nadilson@protonmail.com>
2023-01-21 05:10:15 -0300

    Botão de confirmar

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
-import { View, ScrollView, Text, TextInput } from 'react-native';
+import { View, ScrollView, Text, TextInput, TouchableOpacity } from 'react-native';
 import { BackButton } from '../components/BackButton';
 import { Checkbox } from '../components/Checkbox';
+import { Feather } from '@expo/vector-icons'
+import colors from 'tailwindcss/colors';
 
 const availableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
 
@@ -20,6 +22,7 @@ export function New() {
         <View className="flex-1 bg-background px-8 pt-16">
             <ScrollView
                 showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ paddingBottom: 100 }}
             >
                 <BackButton />
 
@@ -33,6 +36,8 @@ export function New() {
 
                 <TextInput
                     className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600"
+                    placeholder="Exercícios, dormir bem, etc..."
+                    placeholderTextColor={colors.zinc[400]}
                 />
 
                 <Text className="font-semibold mt-4 mb-3 text-white text-base">
@@ -49,6 +54,21 @@ export function New() {
                         />
                     ))
                 }
+
+                <TouchableOpacity
+                    className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
+                    activeOpacity={0.7}
+                >
+                    <Feather
+                        name="check"
+                        size={20}
+                        color={colors.white}
+                    />
+
+                    <Text className="font-semibold text-base text-white ml-2">
+                        Confirmar
+                    </Text>
+                </TouchableOpacity>
             </ScrollView>
         </View>
     )
