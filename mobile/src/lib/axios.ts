7627e4bfd53b691d58eab4ca4d5b66c3064009e4
Nadilson <nadilson@protonmail.com>
2023-01-22 06:24:48 -0300

    Instalação do Axios e configuração do acesso ao server

---
+++ mobile/src/lib/axios.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const api = axios.create({
+    // NOTA: localhost como IP não vai permitir 
+    // que o app funcione no Android. Deve-se adicionar
+    // o IP da máquina onde está rodando o server. Com
+    // o EXPO em execução, é possível ver o IP no log. Outra
+    // forma de descobrir o IP é através do comando ipconfig
+    // no terminal. Deve-se substituir o "localhost" a seguir pelo
+    // IP obtido.
+  baseURL: "http://localhost:3333",
+});
