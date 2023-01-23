511627d49b9fcc4fd7a17db3ac95b856c8625c2a
Nadilson <nadilson@protonmail.com>
2023-01-23 00:19:10 -0300

    Desabilitando em datas passadas

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -1,4 +1,5 @@
 import * as Checkbox from '@radix-ui/react-checkbox';
+import dayjs from 'dayjs';
 import { Check } from 'phosphor-react';
 import { useEffect, useState } from 'react';
 import { api } from '../lib/axios';
@@ -30,6 +31,8 @@ export function HabitsList({ date }: HabitLisProps) {
         })
     }, [])
 
+    const isDateInPast = dayjs(date).endOf('day').isBefore(new Date())
+
     return (
         <div className='mt-6 flex flex-col gap-3'>
             {habitsInfo?.possibleHabits.map(habit => {
@@ -37,6 +40,7 @@ export function HabitsList({ date }: HabitLisProps) {
                     <Checkbox.Root
                         key={habit.id}
                         checked={habitsInfo.completedHabits.includes(habit.id)}
+                        disabled={isDateInPast}
                         className='flex items-center gap-3 group focus:outline-none disabled:cursor-not-allowed'
                     >
                         <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-50 transition-colors group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background'>
