d36c072f9baf31944794920c263d855e2cfc6302
Nadilson <nadilson@protonmail.com>
2023-01-23 00:56:05 -0300

    Sincronizando hábitos completos no front-end

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -5,7 +5,8 @@ import { useEffect, useState } from 'react';
 import { api } from '../lib/axios';
 
 interface HabitLisProps {
-    date: Date
+    date: Date,
+    onCompletedChanged: (completed: number) => void
 }
 
 interface HabitsInfo {
@@ -17,7 +18,7 @@ interface HabitsInfo {
     completedHabits: string[]
 }
 
-export function HabitsList({ date }: HabitLisProps) {
+export function HabitsList({ date, onCompletedChanged  }: HabitLisProps) {
 
     const [habitsInfo, setHabitsInfo] = useState<HabitsInfo>()
 
@@ -31,6 +32,27 @@ export function HabitsList({ date }: HabitLisProps) {
         })
     }, [])
 
+    async function handleToggleHabit(habitId: string) {
+        const isHabitAlreadyCompleted = habitsInfo!.completedHabits.includes(habitId)
+    
+        await api.patch(`/habits/${habitId}/toggle`)
+    
+        let completedHabits: string[] = []
+    
+        if(isHabitAlreadyCompleted) {
+          completedHabits = habitsInfo!.completedHabits.filter(id => id !== habitId)
+        } else {
+          completedHabits = [...habitsInfo!.completedHabits, habitId]
+        }
+    
+        setHabitsInfo({
+          possibleHabits: habitsInfo!.possibleHabits,
+          completedHabits,
+        })
+    
+        onCompletedChanged(completedHabits.length)
+      }
+
     const isDateInPast = dayjs(date).endOf('day').isBefore(new Date())
 
     return (
@@ -39,6 +61,7 @@ export function HabitsList({ date }: HabitLisProps) {
                 return (
                     <Checkbox.Root
                         key={habit.id}
+                        onCheckedChange={() => handleToggleHabit(habit.id)}
                         checked={habitsInfo.completedHabits.includes(habit.id)}
                         disabled={isDateInPast}
                         className='flex items-center gap-3 group focus:outline-none disabled:cursor-not-allowed'
