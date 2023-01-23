6f9b71c429744071dbe4cb317f9c79afbd042e6f
Nadilson <nadilson@protonmail.com>
2023-01-23 00:03:54 -0300

    Listando hábitos possíveis do dia

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -4,6 +4,7 @@ import clsx from "clsx";
 import { ProgressBar } from "./ProgressBar";
 import { Check } from "phosphor-react";
 import dayjs from "dayjs";
+import { HabitsList } from "./HabitsList";
 
 interface HabitDayProps {
   date: Date
@@ -21,7 +22,7 @@ export function HabitDay({ completed = 0, amount = 0, date }: HabitDayProps) {
   return (
     <Popover.Root>
       <Popover.Trigger
-        className={clsx("w-10 h-10border-2 rounded-lg", {
+        className={clsx("w-10 h-10 border-2 rounded-lg", {
           'bg-zinc-900 border-zinc-800': completedPercentage === 0,
           'bg-violet-900 border-violet-700': completedPercentage > 0 && completedPercentage < 20,
           'bg-violet-800 border-violet-600': completedPercentage >= 20 && completedPercentage < 40,
@@ -38,21 +39,7 @@ export function HabitDay({ completed = 0, amount = 0, date }: HabitDayProps) {
 
           <ProgressBar progress={completedPercentage} />
 
-          <div className='mt-6 flex flex-col gap-3'>
-            <Checkbox.Root
-              className='flex items-center gap-3 group'
-            >
-              <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-50'>
-                <Checkbox.Indicator>
-                  <Check size={20} className="text-white" />
-                </Checkbox.Indicator>
-              </div>
-
-              <span className='font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400'>
-                Beber 2L de água
-              </span>
-            </Checkbox.Root>
-          </div>
+          <HabitsList date={date} />
 
           <Popover.Arrow height={8} width={16} className='fill-zinc-900' />
         </Popover.Content>
